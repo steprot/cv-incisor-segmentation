8b42426638cc7ad1d4bd99556ac0f9b3fbc3f552
steprot <stefano.proto@studenti.polito.it>
2017-05-03 16:32:11 +0100

    Read all the landmarks and print them over the radiographs

--- _Code/landmark.py
+++ _Code/landmark.py
@@ -1,37 +1,98 @@
 # -*- coding: utf-8 -*-
+import cv2
+import cv2.cv as cv
 import os
 import numpy as np
+import fnmatch
 
-class Landmark():
-    def __init__(self):
-        # Create the set of points
-        self.points = []
+class Landmarks():
+    
+    def __init__(self, source):
         
-    def readPoints(self, file_path):
-        if file_path is None:
-            print('File location is None.')
+        # Create the set of points
+        self.points = []   
+        if source is not None:
+            self.read_landmark(source)
+        
+    def read_landmark(self, input_file):
+        
+        # Checkt that the input file is not None
+        if input_file is None:
+            print('Input file location is None.')
             return
             
-        # Check that the file exists
-        if not os.path.exists(file_path):
-            print(file_path, ' does NOT not exist.')
+        # Check that the input file exists
+        if not os.path.exists(input_file):
+            print('Input file does not exist.')
             return
+       
+        ## Open the file and read all the lines 
+        #with open(input_file) as f:
+        #    content = f.readlines()
+        #
+        ## Divide the content in a list of numbers. 
+        #content = [x.strip() for x in content]
+        #                
+        #i = 0
+        #points = []
+        #while i < len(content)-1: # -1 because then I add +1
+        #    points.append([float(content[i]), float(content[i+1])])
+        #    i += 2 # To go to the next couple 
+            
+        tmp = open(input_file).readlines()
+        for ind in range(0, len(tmp), 2):
+            self.points.append(np.array([float(tmp[ind + 1].strip()), float(tmp[ind].strip())]))
+                         
+        self.points = np.array(self.points)
+
+    def as_vector(self):
         
-        print('******', file_path, '******')
+        return np.hstack(self.points)
         
-        # Open the file and read all the lines 
-        with open(file_path) as f:
-            content = f.readlines()
-            # Divide the content in a list of numbers. 
-        content = [x.strip() for x in content]
-        print(len(content))
+    def as_matrix(self):
         
-        i = 0
-        for i in range(0, len(content)-1): # -1 because then I add +1
-            np.concatenate((self.points, np.array([float(content[i]), float(content[i+1])])), axis = 0) # DOESN'T WORK
-        
-        print(self.points)
+        return self.points
                     
-    def printLandmark(self):
-        pass
-        print('still TODO')
+
+def render_landmark(landmark):
+        
+    points = landmark.as_matrix()
+    max_y = points[:, 0].max()
+    min_y = points[:, 0].min()
+    max_x = points[:, 1].max()
+    min_x = points[:, 1].min()
+    
+    img = np.zeros((int((max_y - min_y) * 1.1), int((max_x - min_x) * 1.1)))
+    
+    for i in range(len(points)):
+        img[int(points[i, 0] - min_y), int(points[i, 1] - min_x)] = 1
+    
+    cv2.imshow('Rendered shape', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+        
+def render_landmark_over_image(img, landmark):
+    
+    # Transform the array in a matrix 
+    i = 0
+    points = []
+    while i < len(landmark)-1: # -1 because then I add +1
+        points.append([float(landmark[i]), float(landmark[i+1])])
+        i += 2 # To go to the next couple
+    points = np.array(points)
+    
+    for i in range(len(points) - 1):
+        cv2.line(img, (int(points[i, 1]), int(points[i, 0])), (int(points[i + 1, 1]), int(points[i + 1, 0])), (0, 255, 0))
+
+    height = 500
+    scale = height / float(img.shape[0])
+    window_width = int(img.shape[1] * scale)
+    window_height = int(img.shape[0] * scale)
+
+    cv2.namedWindow('rendered image', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('rendered image', window_width, window_height)
+    cv2.imshow('rendered image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    return img
--- _Code/main.py
+++ _Code/main.py
@@ -1,20 +1,55 @@
 # -*- coding: utf-8 -*-
+import cv2
+import cv2.cv as cv
 import os
 import numpy as np
 import fnmatch
-from landmark import Landmark
+from landmark import Landmarks, render_landmark, render_landmark_over_image
 
 if __name__ == '__main__':
     
-    # Read the landmarks       
-    # Specify the name where to read the landmarks from
-    dir_path = os.path.join(os.getcwd(), '../_Data/Landmarks/original/landmarks1-1.txt')
+    # Read the landmarks *******************************************************
+    number_teeth = 8
+    number_samples = 2
     
-    # Create a landmarks object
-    # Each landmark is a tooth from a single person 
-    landmarks = Landmark()
+    teeth_landmarks = np.zeros((number_teeth, number_samples * 80))
+        
+    i = 1
+    while i <= number_teeth: # For all the different teeth 
+        j = 1 
+        tooth_landmarks = np.zeros((number_samples, 80))
+        while j <= number_samples: # For the same tooth but for the different persons
+            
+            # Specify the name where to read the landmarks from
+            directory = '../_Data/Landmarks/original/landmarks' + str(j) + '-' + str(i) + '.txt'
+            dir_path = os.path.join(os.getcwd(), directory)
+            
+            landmark = Landmarks(dir_path)
+            # Print it 
+            #render_landmark(landmark)
+            tooth_landmarks[j-1,:] = landmark.as_vector()
+
+            # Got the next sample
+            j +=1
+            
+        teeth_landmarks[i-1] = np.hstack(tooth_landmarks)
+        # Go to the next tooth
+        i +=1
+        
+    #Print the landmarks over the radiographs **********************************
     
-    # Read the points of the tooth
-    landmarks.readPoints(dir_path)
-    
-    landmarks.printLandmark()
+    i = 0
+    while i < number_samples:
+        j = 0
+        directory = '../_Data/Radiographs/0' + str("%01d" % (i+1)) + '.tif'
+        dir_path = os.path.join(os.getcwd(), directory)
+        img = cv2.imread(dir_path)
+        while j < number_teeth:
+            tooth_landamarks = teeth_landmarks[j,:]
+            tooth_landamarks = tooth_landamarks[i*80:i*80+80]
+            img = render_landmark_over_image(img, tooth_landamarks)
+            
+            # Go to the next tooth
+            j += 1
+        # Got the next sample   
+        i += 1
