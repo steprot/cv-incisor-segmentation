a54136ae273facb6a993c88f3cb28fe9d49b4cfd
steprot <stefano.proto@studenti.polito.it>
2017-05-04 17:56:29 +0100

    centroids and translation to origin

--- _Code/landmark.py
+++ _Code/landmark.py
@@ -52,11 +52,57 @@ class Landmarks():
     def as_matrix(self):
         
         return self.points
-                    
+        
+def compute_centroid(points, weights=None):
+        
+        # Transform the array in a matrix 
+        #i = 0
+        #points = []
+        #while i < len(landmark)-1: # -1 because then I add +1
+        #    points.append([float(landmark[i]), float(landmark[i+1])])
+        #    i += 2 # To go to the next couple
+        #points = np.array(points)
+    
+        if weights is None:
+            centroid = np.mean(points, axis=0)
+        else: 
+            # len(points[0]) is 2
+            centroid = np.zeros((1, len(points[0])))
+            for ind in range(len(points)): # len(points) is 40, so for each pair 
+                centroid += points[ind, :] * weights[ind]
+        return centroid 
+        
+def translate_to_origin(landmark, weights=None):
+    
+        # Transform the array in a matrix 
+        i = 0
+        points = []
+        while i < len(landmark)-1: # -1 because then I add +1
+            points.append([float(landmark[i]), float(landmark[i+1])])
+            i += 2 # To go to the next couple
+        points = np.array(points)
+    
+        if weights is None:
+            centroid = np.mean(points, axis=0)
+        else: 
+            centroid = np.zeros((1, len(points[0])))
+            for ind in range(len(points)):
+                centroid += points[ind, :] * weights[ind]
+            #in case when sum of weights doesn't correspond to 1
+            centroid = centroid.dot(1. / weights.sum()) # Normalizing 
+            
+        points = points - centroid
+
+        #update_centroid
+        #old_centroid = centroid
+        centroid = compute_centroid(points, weights)
+        
+        print(points)
+        return points
 
 def render_landmark(landmark):
         
-        # Transform the array in a matrix 
+    # Transform the array in a matrix 
     i = 0
     points = []
     while i < len(landmark)-1: # -1 because then I add +1
--- _Code/main.py
+++ _Code/main.py
@@ -4,8 +4,26 @@ import cv2.cv as cv
 import os
 import numpy as np
 import fnmatch
-from landmark import Landmarks, render_landmark, render_landmark_over_image
+from landmark import Landmarks, render_landmark, render_landmark_over_image, translate_to_origin, compute_centroid
 from activeshapemodel import ActiveShapeModel
+
+# Print the landmarks over the radiographs *********************************
+def print_landmarks_over_radiographs(teeth_landmarks):
+    i = 0
+    while i < number_samples:
+        j = 0
+        directory = '../_Data/Radiographs/0' + str("%01d" % (i+1)) + '.tif'
+        dir_path = os.path.join(os.getcwd(), directory)
+        img = cv2.imread(dir_path)
+        while j < number_teeth:
+            tooth_landamarks = teeth_landmarks[j,:]
+            tooth_landamarks = tooth_landamarks[i*80:i*80+80]
+            img = render_landmark_over_image(img, tooth_landamarks)
+            
+            # Go to the next tooth
+            j += 1
+        # Got the next sample   
+        i += 1
 
 if __name__ == '__main__':
     
@@ -37,24 +55,9 @@ if __name__ == '__main__':
         # Go to the next tooth
         i +=1
         
-    # Print the landmarks over the radiographs *********************************
-    
-    #i = 0
-    #while i < number_samples:
-    #    j = 0
-    #    directory = '../_Data/Radiographs/0' + str("%01d" % (i+1)) + '.tif'
-    #    dir_path = os.path.join(os.getcwd(), directory)
-    #    img = cv2.imread(dir_path)
-    #    while j < number_teeth:
-    #        tooth_landamarks = teeth_landmarks[j,:]
-    #        tooth_landamarks = tooth_landamarks[i*80:i*80+80]
-    #        img = render_landmark_over_image(img, tooth_landamarks)
-    #        
-    #        # Go to the next tooth
-    #        j += 1
-    #    # Got the next sample   
-    #    i += 1
-        
+    # Print the teeth over the radiographs
+    # print_landmarks_over_radiographs(teeth_landmarks)
+                
     # Fit the ActiveShapeModel and compute the mean sample for each tooth ******
     
     mean_teeth_landmarks = np.zeros((number_teeth, 80))
@@ -65,9 +68,35 @@ if __name__ == '__main__':
         mean_teeth_landmarks[i] = activeShapeModel.compute_mean()
         
         # Print it 
-        render_landmark(mean_teeth_landmarks[i])
+        # render_landmark(mean_teeth_landmarks[i])
         # print(mean_teeth_landmarks)
         i += 1
     
+    # Translate the landmarks to the origin ************************************
     
+    i = 0
+    teeth_landmarks_translated_to_origin = np.zeros((number_teeth, number_samples * 80))
+    while i < number_samples:
+        j = 0
+        while j < number_teeth:
+            tooth_landamarks = teeth_landmarks[j,:]
+            tooth_landamarks = tooth_landamarks[i*80:i*80+80]
+            
+            origin_landmarks = translate_to_origin(tooth_landamarks, None)
+            
+            #teeth_landmarks_translated_to_origin[j, i*80:i*80+80] = compute_centroid(tooth_landamarks, None)
+
+            # Go to the next tooth
+            j += 1
+        # Got the next sample   
+        i += 1
+        
+    mean = compute_centroid(origin_landmarks, None)
+    print(mean)
+        
+    # Print the teeth over the radiographs
+    # print_landmarks_over_radiographs(teeth_landmarks_translated_to_origin)
+        
+        
+        
     
